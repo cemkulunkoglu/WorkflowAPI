8528b522c038621b3a7af95d58a00b936994568a
Cem Külünkoğlu <62657442+cemkulunkoglu@users.noreply.github.com>
2026-01-05 14:34:23 +0300

    Revert "Merge pull request #1 from cemkulunkoglu/codex/create-workflow.messages-microservice"
    This reverts commit ea7d21644beb44a9e2d30a2e377eaa89f3ff188f, reversing
    changes made to 3230baf1dacba8934c49f89aa9350f03041a7c01.

--- Workflow.MessagesService/Services/IEmailSender.cs
+++
@@ -1,8 +0,0 @@
-using Workflow.MessagesService.Entities;
-
-namespace Workflow.MessagesService.Services;
-
-public interface IEmailSender
-{
-    Task SendAsync(OutboxMessage message, CancellationToken cancellationToken);
-}
